d2bc009cccd5ded4529607b8e4f361a75dfd0d32
Abdulhamit Kumru <sahruhdev@gmail.com>
2018-11-03 15:22:42 +0300

    path fonksyonları eklendi

--- main.c
+++ main.c
@@ -12,6 +12,44 @@ char * pathDEV();
 void addPrompt(const char * promptBuf);
 int getPrompt();
 
+char * getParams(const char * promptBuf){
+	int k = 0,i;
+	char * params = malloc(sizeof(char));
+	for (i = 1; promptBuf[i] != '\0'; i++) {
+		if ((int)promptBuf[i] > 96 && (int)promptBuf[i] < 123 ) {
+			params[k] = promptBuf[i];
+			k++;
+			params = realloc(params,k + 1);
+		}
+	}
+	params[k] = '\0';
+	//for (i = 0; i < k; i++) printf("params -> %c \n",params[i] )
+
+	return params;
+}
+
+void lowDEG(const char * promptBuf){
+	int i;
+	char * degPath = pathDEG();
+	FILE * DEG;
+	//printf("kom path ->\"%s\"\n",degPath );
+	DEG = fopen(degPath,"r+");
+	if (DEG == NULL) {
+		printf("!DEGER.TXT ACILAMADI\n");
+		return;
+	}
+	char * params = getParams(promptBuf);
+	int paramSize = 0;
+	//for (i = 0; params[i] != '\0'; i++) printf("params -> %c \n",params[i] )
+	for (i = 0; params[i] != '\0'; i++) paramSize++;
+	printf("paramsize -> %d\n",paramSize );
+	// DOSYAYI ISLEME
+	for (i = 0; i <  ; i++) {
+
+	}
+
+}
+
 int initDEV(const char * promptBuf){
 	char * devPath = pathDEV();
 	FILE * DEV;
@@ -22,6 +60,10 @@ int initDEV(const char * promptBuf){
 	}else{
 		printf("DEVRE.TXT ACILDI\n" );
 	}
+	/*
+
+
+	*/
 	printf("DEVRE.TXT KAPANDI\n" );
 	fclose(DEV);
 	return 1;
@@ -30,7 +72,7 @@ int initDEV(const char * promptBuf){
 		H VE L KOMUTLARINI IMPEMENT ET
 */
 int main(){
-	int i;
+	printf(">>> KAPI SIMULATORU \n" );
 	getPrompt();
 	getPrompt();
 	return 0;
@@ -54,10 +96,15 @@ int getPrompt(){
 	switch (prompt) {
 		case 'y':
 		case 'Y':
-		//printf("y veya Y ye basıldı\n" );
-		addPrompt(promptBuffer);
-		initDEV(promptBuffer);
-		break;
+			//printf("y veya Y ye basıldı\n" );
+			addPrompt(promptBuffer);
+			initDEV(promptBuffer);
+			break;
+		case 'l':
+		case 'L':
+			addPrompt(promptBuffer);
+			lowDEG(promptBuffer);
+			break;
 		default:
 		printf("!HATALI GIRDI\n");
 	}
@@ -71,10 +118,8 @@ void addPrompt(const char * promptBuf){
 	//printf("kom path ->\"%s\"\n",komPath );
 	KOM = fopen(komPath,"a");
 	if (KOM == NULL) {
-		printf("!KOM.TXT ACILAMADI\n");
+		printf("!KOMUT.TXT ACILAMADI\n");
 		return;
-	}else{
-		//printf("KOM.TXT ACILDI\n" );
 	}
 	fputs(promptBuf,KOM);
 	fputc('\n',KOM);
@@ -86,7 +131,7 @@ void addPrompt(const char * promptBuf){
 char * pathDEV(){
 	char cwd[PATH_MAX];
 	if (getcwd(cwd,sizeof(cwd)) == 	NULL) {
-		printf("> devre.txt konuma ulaşılamadı\n");
+		printf("!DEVRE.TXT KONUMUNA ULASILAMADI\n");
 	}else{
 		strcat(cwd,"/devre.txt");
 	}
